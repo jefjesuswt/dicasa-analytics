2e60446bece3f147c018ebd280a3367cb5acc796
Jeffrey Jimenez <jefjesuswt@gmail.com>
2025-11-10 11:22:29 -0400

    health service added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  logger = new Logger('AppService');
+
   getHello(): string {
     return 'Hello World!';
   }
 
   getHealthStatus() {
-    return { status: 'ok', timestamp: new Date().toISOString() };
+    const currentTime = new Date().toISOString();
+    this.logger.log(`Health check, current time: ${currentTime}`);
+    return { status: 'ok', timestamp: currentTime };
   }
 }
